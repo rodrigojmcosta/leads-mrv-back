49a8f1451271877b51b50d8eb44f5b8ce997b6c2
Rodrigo J M Costa <rodrigo.costa@squadra.com.br>
2025-06-26 15:21:13 -0300

    chore: initial project setup with Spring 3.3.13 and Java 21

---
+++ src/main/java/br/com/mrv/leads_mrv/LeadsMrvApplication.java
@@ -0,0 +1,13 @@
+package br.com.mrv.leads_mrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadsMrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadsMrvApplication.class, args);
+	}
+
+}
